965dcc72f245922b7b9538ffa0cfae64b4480851
GarySimons <garycsimons@gmail.com>
2020-05-20 05:55:46 +0000

    Creating blog pages

--- posts/views.py
+++ posts/views.py
@@ -41,6 +41,6 @@ def create_or_edit_post(request, pk=None):
         if form.is_valid():
             post = form.save()
             return redirect(post_detail, post.pk)
-        else:
-            form = BlogPostForm(instance=post)
-        return render(request, 'blogpostform.html', {'form': form})
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blogpostform.html', {'form': form})
