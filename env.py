88589c2045d02761329b945c44b8b6cdaf3b6420
GarySimons <garycsimons@gmail.com>
2020-05-21 07:21:23 +0000

    changes

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "'nj@*2@&$0++4b$mnk^d30j2+*h=qwhf!q-+b&&1xhc7&q27xyi'")
-
-#os.environ.setdefault("DATABASE_URL", "postgres://izppidtonmnueo:e8e764348eb44310947090ef23638b5a8bb2ecb90385033e9e5e4ea1a7b0b2d8@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/dbtkksqens8ohm")
